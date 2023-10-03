caae0fb86c18c5d5a5fd745b2863103c14efbeab
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2023-10-03 19:07:50 -0300

    use efficientnet and accept batch_size

--- rsna_atd/models.py
+++ rsna_atd/models.py
@@ -29,7 +29,7 @@ class CT_BaseModel(pl.LightningModule):
             weight = torch.Tensor(list(loss_weights[head_name].values()))
             self.loss_functions[head_name] = nn.CrossEntropyLoss(weight=weight)
 
-        self.hparams.lr = lr
+        self.lr = lr
 
     def forward(x):
         B = len(x)
@@ -90,7 +90,7 @@ class CT_BaseModel(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.hparams.lr)
+        optimizer = optim.Adam(self.parameters(), lr=self.lr)
         total_train_steps = self.trainer.estimated_stepping_batches
         warmup_steps = int(total_train_steps * 0.10)
         decay_steps = total_train_steps - warmup_steps
@@ -161,10 +161,33 @@ class CT_2DModel(CT_BaseModel):
         outputs = outputs + [any_injury]
 
         return outputs
+    
+# block args
+blocks_args_str = [
+    "r1_k3_s22_e1_i32_o16_se0.25", # This one is modified from s11 to s22, meaning stride 2
+    "r2_k3_s22_e6_i16_o24_se0.25",
+    "r2_k5_s22_e6_i24_o40_se0.25",
+    "r3_k3_s22_e6_i40_o80_se0.25",
+    "r3_k5_s11_e6_i80_o112_se0.25",
+    "r4_k5_s22_e6_i112_o192_se0.25",
+    "r1_k3_s11_e6_i192_o320_se0.25",
+]
+
+efficientnet_dict = {
+    "efficientnet-b0": {"width_mult": 1.0, "depth_mult": 1.0, "image_size": 224, "dropout_rate": 0.2, "dropconnect_rate": 0.2},
+    "efficientnet-b1": {"width_mult": 1.0, "depth_mult": 1.1, "image_size": 240, "dropout_rate": 0.2, "dropconnect_rate": 0.2},
+    "efficientnet-b2": {"width_mult": 1.1, "depth_mult": 1.2, "image_size": 260, "dropout_rate": 0.3, "dropconnect_rate": 0.2},
+    "efficientnet-b3": {"width_mult": 1.2, "depth_mult": 1.4, "image_size": 300, "dropout_rate": 0.3, "dropconnect_rate": 0.2},
+    "efficientnet-b4": {"width_mult": 1.4, "depth_mult": 1.8, "image_size": 380, "dropout_rate": 0.4, "dropconnect_rate": 0.2},
+    "efficientnet-b5": {"width_mult": 1.6, "depth_mult": 2.2, "image_size": 456, "dropout_rate": 0.4, "dropconnect_rate": 0.2},
+    "efficientnet-b6": {"width_mult": 1.8, "depth_mult": 2.6, "image_size": 528, "dropout_rate": 0.5, "dropconnect_rate": 0.2},
+    "efficientnet-b7": {"width_mult": 2.0, "depth_mult": 3.1, "image_size": 600, "dropout_rate": 0.5, "dropconnect_rate": 0.2},
+    "efficientnet-b8": {"width_mult": 2.2, "depth_mult": 3.6, "image_size": 672, "dropout_rate": 0.5, "dropconnect_rate": 0.2},
+    "efficientnet-l2": {"width_mult": 4.3, "depth_mult": 5.3, "image_size": 800, "dropout_rate": 0.5, "dropconnect_rate": 0.2}
+}
 
 class CT_3DModel(CT_BaseModel):
-    """ 3D model for CT scans. As input, the model expects a batch of volumes of size (B, C, H, W, D). Where C
-    corresponds to the number of sessions. The logic is that the backbone extracts a representation for each session,
+    """ 3D model for CT scans. The idea of the model is that the backbone extracts a representation for each session,
     and then the aggregator combines the representations into a single representation. This representation is then
     used by the heads to predict the probabilities for each class.
 
@@ -174,20 +197,15 @@ class CT_3DModel(CT_BaseModel):
     a softmax activation function. The output of the model is a list of 6 tensors, one for each head. The last
     head, which corresponds to no injury, is computed from the injury probabilities of the other heads.
     The loss function is a weighted cross entropy loss.
-
-    NOTE: The model is constructed to only accept batchsizes of 1. This is because input sizes may vary and passing multiple
-    volumes of different sizes to the model would be problematic.
-
     """
     def __init__(self, lr=1e-4, loss_weights=None):
         super().__init__(loss_weights=loss_weights, lr=lr)
 
-        out_channels = 2 # This doesn't matter as the class_layers will be overriden
-        self.backbone = monai.networks.nets.DenseNet121(spatial_dims=3, in_channels=1, out_channels=out_channels)
-        self.backbone._modules["class_layers"] = nn.Sequential(nn.ReLU(), nn.AdaptiveAvgPool3d(1), nn.Flatten())
+        self.backbone  = monai.networks.nets.EfficientNet(blocks_args_str=blocks_args_str, spatial_dims=3, in_channels=1)
+        self.backbone._fc = nn.Identity()
 
-        rnn_input_size = 1024 + 1 # number of output channels of backbone + aortic_hu
-
+        rnn_input_size = 1280 + 1 # number of channels of backbone + aortic_hu
+ 
         hidden_size = config.RNN_HIDDEN_SIZE
         self.aggregator = nn.RNN(input_size=rnn_input_size, 
                                  hidden_size=hidden_size, 
@@ -212,17 +230,30 @@ class CT_3DModel(CT_BaseModel):
         ])
 
     def forward(self, x, aortic_hu):
-        # x.shape: B, C, H, W, D
-        x = torch.swapaxes(x, 0, 1)
-        x = self.backbone(x)
-        # aortic_hu.shape: B, C
-        aortic_hu = torch.swapaxes(aortic_hu, 0, 1)
-        x = torch.concat((x, aortic_hu), dim=-1)
-        rnn_outputs, _ = self.aggregator(x)
-        x = rnn_outputs[None, -1]
+        """ Forward pass of the model.
+
+        Args:
+            x: list of tensors of shape (b, C, H, W, D) with length B
+            aortic_hu: list of tensors of shape (b, C) with length B
         
+        Returns:
+            outputs: list of tensors of shape (B, 2), (B, 2), (B, 3), (B, 3), (B, 3), (B, 1)
+
+        Note: b = 1 (placeholder unitary batch size)
+        """
+        n_sessions = [len(volume) for volume in x]
+#         x = max_shape_pad(x)
+        x = torch.cat(x, dim=0) # concatenate along channel dim
+        x = x.unsqueeze(1) # add channel dim
+        x = self.backbone(x)
+        x = torch.split(x, n_sessions, dim=0) # split along channel dim
+        # concatenate the aortic_hu to the backbone output
+        x = [torch.cat([volume, hu], dim=-1) for volume, hu in zip(x, aortic_hu)]
+        # pass each volume to the aggregator separately and get the last hidden state
+        x = torch.stack([self.aggregator(volume)[0][-1, :] for volume in x])
+
         outputs = [head(x) for head in self.heads]
-        # P[no_injury] = P[no_injury_bowel] * P[no_injury_extra] * P[no_injury_liver] * P[no_injury_kidney] * P[no_injury_spleen]
+        # P[no_injury] = Prod(P[no_injury_{task}] for task in tasks)
         no_injury = torch.prod(torch.stack([output[:, 0] for output in outputs], dim=-1),
                                dim=-1, keepdim=True)
         any_injury = 1-no_injury
@@ -237,7 +268,9 @@ class CT_3DModel(CT_BaseModel):
     
     def predict_step(self, batch, batch_idx):
         x, aortic_hu = batch
-        return self.forward(x, aortic_hu)
+        x = self.forward(x, aortic_hu)
+        x = torch.concat(x, 1)
+        return x
 
     def configure_optimizers(self):
         # NOTE: We use SGD as Adam has issues with float16
